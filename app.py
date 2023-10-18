a76c9ec7635d51c12490b1b11274a013a7d7b37a
Prabin K C <pkmiracle36@gmail.com>
2023-10-19 09:15:33 +1100

    correction

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, this is the Flask app running on Server 2!"
+    return "Hello, this is the Flask app running on a dedicated server!"
